e9714090a9f8ae9c3fd62b83e6f2e57d6a166651
ejimenez123 <caribestic123@gmail.com>
2022-01-14 13:42:40 -0500

    feat: 04-Event Handling

---
+++ 04-event-handling/src/Counter.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const count = ref(0)
+const isEven = computed(()=> count.value % 2 === 0);
+const emit = defineEmits(['increment'])
+const handleClick = () => {
+  count.value += 1
+  //c.emit('increment', this.count)
+  emit('increment', {
+    count: count.value,
+    isEven: isEven.value
+  })
+}
+</script>
+
+<template>
+  <button @click="handleClick">Increment</button>
+  {{ count }} {{ count ? isEven : '' }} 
+</template>
+
+<!--script>
+/* Composition API
+import { ref, computed } from 'vue'
+
+export default {
+  setup(p,c) {      
+    const count = ref(0)
+    const isEven = computed(()=> count.value % 2 === 0);
+    const handleClick = () => {
+      count.value += 1
+      //c.emit('increment', this.count)
+      c.emit('increment', {
+        count: count.value,
+        isEven: isEven.value
+      })
+      
+    }
+    return {
+      count,
+      isEven,
+      handleClick
+    };      
+  }
+};
+*/ 
+
+/* Options API
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+   isEven () {
+     return this.count % 2 === 0
+   }
+  },
+  methods: {
+    handleClick() {
+      this.count += 1
+      //this.$emit('increment', this.count)
+      this.$emit('increment', {
+        count: this.count,
+        isEven: this.isEven
+      })
+      
+    }
+  }
+}*/
+</script-->
+
+  
---
+++ 04-event-handling/test/app.spec.js
@@ -0,0 +1,48 @@
+import { mount } from '@vue/test-utils'
+import Counter from '../src/Counter.vue'
+
+test('emits an event when clicked', () => {
+  const wrapper = mount(Counter)
+
+  wrapper.find('button').trigger('click')
+  wrapper.find('button').trigger('click')
+
+  expect(wrapper.emitted()).toHaveProperty('increment')
+})
+
+/*test('emits an event with count when clicked', () => {
+  const wrapper = mount(Counter)
+
+  wrapper.find('button').trigger('click')
+  wrapper.find('button').trigger('click')
+  const incrementEvent = wrapper.emitted('increment')
+  
+  expect(incrementEvent).toHaveLength(2)
+  expect(incrementEvent[0]).toEqual([1])
+  expect(incrementEvent[1]).toEqual([2])
+})*/
+
+test('emits an event with count when clicked', () => {
+  const wrapper = mount(Counter)
+
+  wrapper.find('button').trigger('click')
+  wrapper.find('button').trigger('click')
+
+  expect(wrapper.emitted('increment')).toHaveLength(2)
+
+  expect(wrapper.emitted('increment')[0]).toEqual([
+    {
+      count: 1,
+      isEven: false
+    }
+  ])
+
+  expect(wrapper.emitted('increment')[1]).toEqual([
+    {
+      count: 2,
+      isEven: true
+    }
+  ])
+})
+
+
